e94cc36a3ac26e7d423ffe1221120cb11518a213
包饭厅 <1316149505@qq.com>
2024-04-17 14:33:04 +0800

    2.2 鼠标拖动窗口

--- onlinemp3widget.cpp
+++ onlinemp3widget.cpp
@@ -23,3 +23,102 @@ void OnlineMp3Widget::paintEvent(QPaintEvent *event)
     QPainter painter(this);
     //painter.drawPixmap(0,0,width(),height(),QPixmap(":/res/dingyilang.png"));
 }
+
+//移动事件
+void OnlineMp3Widget::mouseMoveEvent(QMouseEvent *event)
+{
+    if (mousePress)  // 如果鼠标被按下
+    {
+        QPoint movepos = event->globalPos();  // 获取鼠标当前的全局位置
+        move(movepos - movePoint);  // 移动窗口位置
+    }
+}
+
+//鼠标释放事件
+void OnlineMp3Widget::mouseReleaseEvent(QMouseEvent *event)
+{
+    Q_UNUSED(event)  // 避免未使用的参数警告
+    mousePress = false;  // 鼠标释放，标记鼠标状态为未按下
+}
+
+//鼠标点击事件
+void OnlineMp3Widget::mousePressEvent(QMouseEvent *event)
+{
+    if (event->button() == Qt::LeftButton)  // 如果鼠标左键被按下
+    {
+        mousePress = true;  // 标记鼠标状态为按下
+    }
+    movePoint = event->globalPos() - pos();  // 计算鼠标相对于窗口左上角的偏移量
+}
+
+void OnlineMp3Widget::on_btn_close_clicked()
+{
+    this->close();
+}
+
+
+void OnlineMp3Widget::on_btn_min_clicked()
+{
+
+}
+
+
+void OnlineMp3Widget::on_btn_change_clicked()
+{
+
+}
+
+
+void OnlineMp3Widget::on_btn_search_clicked()
+{
+
+}
+
+
+void OnlineMp3Widget::on_btn_lastsong_clicked()
+{
+
+}
+
+
+void OnlineMp3Widget::on_btn_start_stop_clicked()
+{
+
+}
+
+
+void OnlineMp3Widget::on_btn_nextsong_clicked()
+{
+
+}
+
+
+void OnlineMp3Widget::on_btn_loop_clicked()
+{
+
+}
+
+
+void OnlineMp3Widget::on_hs_sound_valueChanged(int value)
+{
+
+}
+
+
+void OnlineMp3Widget::on_hs_songtime_sliderPressed()
+{
+
+}
+
+
+void OnlineMp3Widget::on_hs_songtime_sliderReleased()
+{
+
+}
+
+
+void OnlineMp3Widget::on_hs_songtime_valueChanged(int value)
+{
+
+}
+
--- onlinemp3widget.h
+++ onlinemp3widget.h
@@ -2,21 +2,24 @@
 #define ONLINEMP3WIDGET_H
 
 #include <QWidget>
-#include <QNetworkRequest> //HTTP的URL管理类
-#include <QNetworkAccessManager>
-#include <QNetworkReply>
-#include <QEventLoop>
-#include <QJsonArray>
-#include <QJsonObject>
-#include <QMediaPlayer>
+#include <QNetworkRequest>          //HTTP的URL管理类
+#include <QNetworkAccessManager>    //URL的上传管理
+#include <QNetworkReply>            //网页回复数据触发信号的类
+#include <QEventLoop>               //提供一种进入和离开事件循环的方法
+#include <QJsonArray>               //封装JSON数组
+#include <QJsonObject>              //封装JSON对象
+
+#include <QMediaPlayer>             //播放音乐相关
 #include <QMediaPlaylist>
-#include <QSqlDatabase>
+
+#include <QSqlDatabase>             //数据库相关
 #include <QSqlQuery>
 #include <QSqlError>
+
 #include <QMessageBox>
 #include <QTime>
 #include <math.h>
-
+#include <QMouseEvent>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -34,7 +37,29 @@ public:
 
     void paintEvent(QPaintEvent *event);
 
+    void mouseMoveEvent(QMouseEvent *event);
+    void mouseReleaseEvent(QMouseEvent *event);
+    void mousePressEvent(QMouseEvent *event);
+
+
+private slots:
+    void on_btn_close_clicked();                //关闭窗口
+    void on_btn_min_clicked();                  //最小化窗口
+    void on_btn_change_clicked();               //更换皮肤
+    void on_btn_search_clicked();               //搜索歌曲
+    void on_btn_lastsong_clicked();             //上一首
+    void on_btn_start_stop_clicked();           //播放
+    void on_btn_nextsong_clicked();             //下一首
+    void on_btn_loop_clicked();                 //循环播放
+    void on_hs_sound_valueChanged(int value);   //音量调节
+
+    void on_hs_songtime_sliderPressed();        //播放进度条调节
+    void on_hs_songtime_sliderReleased();
+    void on_hs_songtime_valueChanged(int value);
+
 private:
     Ui::OnlineMp3Widget *ui;
+    bool mousePress = false;
+    QPoint movePoint;
 };
 #endif // ONLINEMP3WIDGET_H
